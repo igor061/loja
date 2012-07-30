22c80b09ab5c748b064d9937598cae346f799299
Igor Miranda <igor061@gmail.com>
2012-07-30 15:18:48 -0300

    adicionando o controle de banco do heroku

--- src/settings.py
+++ src/settings.py
@@ -9,16 +9,13 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
-}
+from unipath import Path
+PROJECT_DIR = Path(__file__).parent
+
+
+import dj_database_url
+DATABASES = {'default': dj_database_url.config(default='sqlite:///'+PROJECT_DIR+'/eventex.db')}
+
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
